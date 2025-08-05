973d780b3741b1f6559b32487e9f930dbdc8aaac
BUTTON-BASE <buttonbotbase@gmail.com>
2025-08-05 16:54:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~WkRUXRrR#NCX_Di6EVFydVd-WN5Jk3lbVA9bckzzZpjnuOH6eOEc",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
